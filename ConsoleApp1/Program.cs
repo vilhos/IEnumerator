04d2cb9c7ae4bcc732c467216de20e7f44ab5787
Vilen <abyancz@gmail.com>
2021-10-04 19:29:35 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = new Day();
+            Date date = new Date();
+            Week week = new Week(date,day);
+
+            foreach (int num1 in date)
+            {
+                Console.WriteLine(num1);
+            }
+
+            foreach (string num2 in day)
+            {
+                Console.WriteLine(num2);
+            }
+
+            foreach (KeyValuePair<Date, Day> keyValue in week)
+            {
+                foreach (var key in keyValue.Key)
+                {
+                    foreach (var value in keyValue.Value)
+                    {
+                        Console.WriteLine($"{ key} {value}");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Day
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        public IEnumerator GetEnumerator()
+        {
+            return new DayEnumerator(days);
+        }
+    }
+
+    class DayEnumerator : IEnumerator
+    {
+        string[] _days;
+        int position = -1;
+        public DayEnumerator(string[] days)
+        {
+            _days = days;
+        }
+        public object Current => _days[position];
+
+        public bool MoveNext()
+        {
+            if (position < _days.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+
+    class Date : IEnumerable
+    {
+        int[] date = {1,2,3,4,5,6,7};
+
+        public IEnumerator GetEnumerator()
+        {
+            return new DateEnumerator(date);
+        }
+    }
+
+    class DateEnumerator : IEnumerator
+    {
+        int[] _date;
+        int position = -1;
+        public DateEnumerator(int[] date)
+        {
+            _date = date;
+        }
+        public object Current => _date[position];
+
+        public bool MoveNext()
+        {
+            if (position < _date.Length-1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+
+    class Week
+    {
+        Dictionary<Date, Day> week = new Dictionary<Date, Day>();
+
+        public Week(Date date, Day day)
+        {
+            week.Add(date, day);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return week.GetEnumerator();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var university = new University();
+            var ministryOfEducation = new MinistryOfEducation(university);
+
+            ministryOfEducation.GenerateStudents();
+            List<StudentData> studentDatas = new List<StudentData>();
+
+            foreach (var student in ministryOfEducation)
+            {
+                studentDatas.Add(new StudentData(student.Name, student.Year));
+                Console.WriteLine($"Name = {student.Name}  Year = {student.Year}  Country = {student.Country}");
+            }
+        }
+    }
+
+    class University : IEnumerable<Student>
+    {
+        private List<Student> _students;
+
+        public University()
+        {
+            _students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            return _students.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Year;
+        public Countries Country;
+    }
+
+    class MinistryOfEducation : IEnumerable<Student>
+    {
+        private University _university;
+
+        public MinistryOfEducation(University university)
+        {
+            _university = university;
+        }
+
+        public void GenerateStudents()
+        {
+            _university.AddStudent(new Student() { Name = "Vazgo", Year = 1987, Country = Countries.Armenia });
+            _university.AddStudent(new Student() { Name = "Kolya", Year = 2001, Country = Countries.Russia });
+            _university.AddStudent(new Student() { Name = "Klimax", Year = 1960, Country = Countries.Russia });
+            _university.AddStudent(new Student() { Name = "Klitor", Year = 2000, Country = Countries.USA });
+            _university.AddStudent(new Student() { Name = "Katya", Year = 1978, Country = Countries.Russia });
+            _university.AddStudent(new Student() { Name = "Aram", Year = 1988, Country = Countries.Armenia });
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            return _university.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class StudentData
+    { 
+        public string Name;
+        public int Year;
+
+        public StudentData(string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+    }
+
+    public enum Countries
+    {
+        Armenia,
+        Russia,
+        USA
+    }
+}
